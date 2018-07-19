b8f61e6d088d99d17dd88555cadd766e91e5eda5
twocucao <twocucao@gmail.com>
2018-07-19 14:23:58 +0800

    fix

--- install_non_brew_packages.sh
+++ install_non_brew_packages.sh
@@ -1,7 +1,10 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 
-# Update hombrew.
+# Update brew and software installed by hombrew.
 brew update && brew upgrade && brew cleanup && brew prune
+
+# Python 2 utilities.
+pip install --upgrade neovim
 
 # Python 3 utilities.
 pip3 install --upgrade legit
@@ -20,3 +23,17 @@ mas upgrade
   git fetch --tags origin
   git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
 ) && \. "$NVM_DIR/nvm.sh"
+
+# install nvm first
+# https://github.com/creationix/nvm
+
+npm install -g cnpm --registry=https://registry.npm.taobao.org
+npm install -g yarn
+npm install -g prettier
+npm install -g eslint-plugin-vue
+npm install -g eslint
+npm install -g gitbook-cli
+npm install -g hexo-cli
+npm install -g speed-test
+npm install -g vue-cli
+
